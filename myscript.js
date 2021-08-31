5b4e34cd3a914af98044d9a2f65867a887c95d3f
WalterFrongia <walter.frongia@libero.it>
2021-08-31 21:23:09 +0200

    Finalizzata prima parte esercizio e inizziata seconda parte

---
+++ myscript.js
@@ -0,0 +1,35 @@
+// Palidroma
+
+
+// 1 Chiedere all’utente di inserire una parola
+
+let parolaUtente = prompt("inserisci una parola");
+console.log(parolaUtente);
+
+// 2 Creare una funzione per capire se la parola inserita è palindroma
+ 
+function checkPalindrom (str) {
+    return str == str.split('').reverse().join('');
+  }
+
+  console.log(checkPalindrom(parolaUtente));
+
+
+// 3 Pari e Dispari
+
+let pariODispari = prompt("scegli pari o dispari")
+
+// 4 L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+
+
+
+// 5 Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+
+
+// 6 Sommiamo i due numeri
+
+
+// 7 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+
+
+// 8 Dichiariamo chi ha vin
